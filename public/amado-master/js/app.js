9225d9d91b0322d019def2b5916858cc4dd0d1bb
sarun <sarun@LAPTOP-UDBQ7O38>
2019-10-15 17:21:30 +0700

    fix filter

--- public/amado-master/js/app.js
+++ public/amado-master/js/app.js
@@ -1,25 +1,10 @@
 //require('./bootstrap');
+var tableproduct = "";//All product List in JSON
 
 function showProductList(json){
-    var tableproduct = "";
     //var i = 0;
     //var json = $jsonProduct;
     json.forEach( function(a) {
-    //     document.getElementById("productArea").insertAdjacentHTML("afterend",`
-    //         <div class="single-products-catagory">
-    //             <a href="shop.html">
-    //                 <img src="./amado-master/img/bg-img/1.jpg" alt="">
-    //                 <!-- Hover Content -->
-    //                 <div class="hover-content">
-    //                     <div class="line"></div>
-    //                     <p>In Stock ${a.quantityInStock} </p>
-    //                     <p>price ${a.MSRP}</p>
-    //                     <h4>${a.productName}</h4>
-    //                 </div>
-    //              </a>
-    //         </div>
-    //         `);
-    // });
     tableproduct += `
         <div class="single-products-catagory">
             <a href="shop.html">
@@ -28,8 +13,21 @@ function showProductList(json){
                 <div class="hover-content">
                     <div class="line"></div>
                     <p>In Stock ${a.quantityInStock} </p>
-                    <p>price ${a.MSRP}</p>
+                    <p>price ${a.buyPrice}</p>
+                    <h5>${a.productScale}</h5>
+                    <h5>${a.productVendor}</h5>
                     <h4>${a.productName}</h4>
+                </div>
+                <div class="pdDetail" style= "display:none">
+                    <p>${a.productCode}</p>
+                    <p>${a.productName}</p>
+                    <p>${a.productLine}</p>
+                    <p>${a.productScale}</p>
+                    <p>${a.productVendor}</p>
+                    <p>${a.productDescription}</p>
+                    <p>${a.quantityInStock}</p>
+                    <p>${a.buyPrice}</p>
+                    <p>${a.MSRP}</p>
                 </div>
             </a>
         </div>
@@ -41,41 +39,28 @@ function showProductList(json){
 
 
 function filterByProductName() {
-    //document.getElementById("productArea").innerHTML = tableproduct;
-    var input, filter, slot, howercontent, pdName, i, txtValue, a
-    var newTableList = "", instock, price;
-    input = document.getElementById("myInput");
+    document.getElementById("productArea").innerHTML = tableproduct;
 
+    var input, filter, slot, single_products_catagory, pdName, i, txtValue, a;
+
+    input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
+
     slot = document.getElementById("productArea");
-    howercontent = slot.getElementsByClassName("single-products-catagory");
-    for (i = 0; i < howercontent.length; i++) {
-         a = howercontent[i].getElementsByTagName("a")[0];
+    single_products_catagory = slot.getElementsByClassName("single-products-catagory");
+    for (i = 0; i < single_products_catagory.length; i++) {
+        //single_products_catagory[i].style.position="absolute";
+        a = single_products_catagory[i].getElementsByTagName("a")[0];
+        pdDetail = a.getElementsByClassName("pdDetail");
         pdName = a.getElementsByTagName("h4")[0];
-        //instock = a.getElementsByTagName("p")[0].innerText;
-        //price = a.getElementsByTagName("p")[1].innerText;
         if (pdName) {
             txtValue = pdName.textContent || pdName.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                howercontent[i].style.display = "";
-                            // newTableList += `
-                            //     <div class="single-products-catagory">
-                            //         <a href="shop.html">
-                            //             <img src="./amado-master/img/bg-img/1.jpg" alt="">
-                            //             <div class="hover-content">
-                            //                 <div class="line"></div>
-                            //                 <p>${instock} </p>
-                            //                 <p>${price}</p>
-                            //                 <h4>${txtValue}</h4>
-                            //             </div>
-                            //         </a>
-                            //     </div>
-                            // `
+                single_products_catagory[i].style.display = "";
+                // document.getElementById("productArea").insertAdjacentHTML("afterend", filteredList);
             } else {
-                howercontent[i].style.display = "none";
+                single_products_catagory[i].style.display = "none";
             }
         }
     }
-    //document.getElementById("productArea2").innerHTML = newTableList;
-
 }
