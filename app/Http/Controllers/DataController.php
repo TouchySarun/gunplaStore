ae2a1568e6b8cfebef82be2e7d8b45a2b4efcac0
SIRIN RATTANARUANG-AMPHAI <sirin_r@cmu.ac.th>
2019-12-04 17:41:57 +0700

    order detail

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -111,7 +111,7 @@ class DataController extends Controller
         return [json_encode($address),json_encode($point)];
     }
     public function addAddress(Request $request, $code){
-        DB::insert("insert into addresses(addressLine1,addressLine1,city,state,postalCode,country,customerNumber,addressNumber,deleteFlag)
+        DB::insert("insert into addresses(addressLine1,addressLine2,city,state,postalCode,country,customerNumber,addressNumber,deleteFlag)
             values ('$request->addrline1','$request->addrline2','$request->city','$request->state','$request->postalcode','$request->country','$code','$request->addrnum','false')" );
         $product = DB::select("select * from addresses where customerNumber = '$code'");
         $jsonp = json_encode($product);
